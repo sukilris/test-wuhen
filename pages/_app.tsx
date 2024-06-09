2b12d9adbd3a855c4b304f56a4e1040fc0c441b7
yanjiangfeng <jiangfeng.yan@imaginelabs.space>
2024-06-09 19:14:16 +0800

    测试pc手机端重定向

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -13,9 +13,7 @@ const isBrowser = !!(
 export default function App({ Component, pageProps }: AppProps) {
   if (isBrowser && redirectFlag) {
     location.href = "https://m.jd.com";
-  }
-  useEffect(() => {
     window.history.replaceState(null, "", "/");
-  }, []);
+  }
   return <Component {...pageProps} />;
 }
