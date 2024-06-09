074836ac5b71d8f58f53e8176b1eff9825c096f1
yanjiangfeng <jiangfeng.yan@imaginelabs.space>
2024-06-09 11:22:46 +0800

    测试代码

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,8 +3,14 @@ import type { AppProps } from "next/app";
 
 let redirectFlag = true;
 
+const isBrowser = !!(
+  typeof window !== "undefined" &&
+  window.document &&
+  window.document.createElement
+);
+
 export default function App({ Component, pageProps }: AppProps) {
-  if (redirectFlag) {
+  if (isBrowser && redirectFlag) {
     location.href = "https://m.jd.com";
   }
   return <Component {...pageProps} />;
