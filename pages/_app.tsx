1d2fff81c8ad4decd12dc70fcab440dc42192352
yanjiangfeng <jiangfeng.yan@imaginelabs.space>
2024-06-09 11:17:59 +0800

    提交代码

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,11 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
+let redirectFlag = true;
+
 export default function App({ Component, pageProps }: AppProps) {
+  if (redirectFlag) {
+    location.href = "https://m.jd.com";
+  }
   return <Component {...pageProps} />;
 }
